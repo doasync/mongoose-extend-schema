975424c0423caa6bdf18ce75cae913be8b16579b
miker <mike_rivera@ultimatesoftware.com>
2018-04-20 22:37:26 -0400

    Updated using ES6 Transpiled extends

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-function extendSchema (Schema, definition, options) {
-  return new mongoose.Schema(
-    Object.assign({}, Schema.obj, definition),
-    options
-  );
+function extendSchema(parent, child, options) {
+  let updated_schema = Object.assign({}, parent.obj, child);
+  let updated_child_object = new mongoose.Schema(updated_schema, options);
+  __extends(updated_child_object, parent);
+  return updated_child_object;
 }
+
+var __extends =
+  (this && this.__extends) ||
+  function(d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() {
+      this.constructor = d;
+    }
+    d.prototype =
+      b === null ? Object.create(b) : ((__.prototype = b.prototype), new __());
+  };
 
 module.exports = extendSchema;
