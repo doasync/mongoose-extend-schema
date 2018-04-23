e6abd670f15999b8a9fc0c717687e6b4945ee1a0
miker <mike_rivera@ultimatesoftware.com>
2018-04-23 13:26:19 -0400

    Updates to handle arrays

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-
 const mongoose = require("mongoose");
 
 function extendSchema(parent, child, options) {
-  let updated_schema = Object.assign(parent.obj, child);
+  let updated_schema = Object.assign({}, parent.obj, child);
   let updated_child_object = new mongoose.Schema(updated_schema, options);
   __extends(updated_child_object, parent);
   return updated_child_object;
@@ -11,7 +10,16 @@ function extendSchema(parent, child, options) {
 var __extends =
   (this && this.__extends) ||
   function(d, b) {
-    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    for (var p in b)
+      if (b.hasOwnProperty(p)) {
+        //array
+        if (Array.isArray(b[p]) && Array.isArray(d[p])) {
+          d[p] = Array.from(new Set(d[p].concat(b[p])));
+        } else {
+          //actual object
+          d[p] = Object.assign({}, b[p], d[p]);
+        }
+      }
     function __() {
       this.constructor = d;
     }
